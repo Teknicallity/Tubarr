a147b960ea99df39e2a51fae57ab353368b30b23
Teknicallity <joshua.s.3322@gmail.com>
2024-06-08 14:53:48 -0400

    blank url path redirects to /channels/

--- Tubarr/urls.py
+++ Tubarr/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('channels/', include("videomanager.urls"))
+    path('', RedirectView.as_view(pattern_name='videomanager:index')),
+    path('channels/', include('videomanager.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
